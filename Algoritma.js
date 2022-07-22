b2ef555d6bdfd3ee791021372265a28674873e0f
Klv168 <marcelo.kelvinn@gmail.com>
2022-07-23 00:52:25 +0700

    SOAL 1

---
+++ Algoritma.js
@@ -0,0 +1,25 @@
+// SOAL 1
+
+// Untuk menghitung massa jenis sebuah benda diperlukan rumus berikut:
+// rho = massa/ volume
+// tentukan algoritma dan pseudocode untuk menghitung rumus tersebut
+
+// // Keterangan
+
+// var massa = 9; 
+// var volume = 3;
+
+// console.log (massa/volume)
+
+
+// SOAL 2
+// Diketahui rumus luas permukaan tabung adalah 
+// luas_permukaan_tabung = (2 x phi x tinggi x r) + (2 x phi x r x r)
+
+// tentukan algoritma dan pseudocode untuk menghitung rumus tersebut
+
+// var phi = 22/7;
+// var tinggi = 10;
+// var r = 7;
+
+// console.log (2 * phi * tinggi * r) + (2 * phi * r * r)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Latihan-day1-selfmade
-Trying to recreate own repository
+Assignments Kelvin Marcello
+Algoritma dan Pseudocode
